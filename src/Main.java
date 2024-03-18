c0aa59f80a8bc5766266dd5e8fd360f0009aa36c
Asif Alli <alli.asif@gmail.com>
2024-03-18 02:04:55 -0400

    Added code to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import com.alliconsulting.SimpleIJJavaApp.Fib;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Fib World!");
+
+        System.out.println("Fib(7) = " + new Fib().DoFib(7));
     }
 }
