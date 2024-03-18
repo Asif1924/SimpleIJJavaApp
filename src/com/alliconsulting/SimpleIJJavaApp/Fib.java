a11c8a35dba16567e9b3d1073c9e9d0d8c119904
Asif Alli <alli.asif@gmail.com>
2024-03-18 02:00:41 -0400

    Added implementation and tests

--- src/com/alliconsulting/SimpleIJJavaApp/Fib.java
+++ src/com/alliconsulting/SimpleIJJavaApp/Fib.java
@@ -2,4 +2,10 @@ package com.alliconsulting.SimpleIJJavaApp;
 
 public class Fib {
 
+    public int DoFib(int argNum) {
+        if(argNum<2)
+            return argNum;
+        else
+            return DoFib(argNum-1) + DoFib(argNum-2);
+    }
 }
--- test/com/alliconsulting/SimpleIJJavaApp/Tests/FibTest.java
+++ test/com/alliconsulting/SimpleIJJavaApp/Tests/FibTest.java
@@ -18,5 +18,84 @@ public class FibTest {
         //Assert.assertEquals(fib,null);
         Assert.assertTrue(fib!=null);
     }
+
+    @Test
+    public void test_fib_0_equals_0(){
+        Fib fib = new Fib();
+
+        int x = fib.DoFib(0);
+
+
+        //Assert.assertEquals(fib,null);
+        Assert.assertEquals(x,0);
+    }
+
+
+    @Test
+    public void test_fib_1_equals_1(){
+        Fib fib = new Fib();
+
+        int x = fib.DoFib(1);
+
+
+        //Assert.assertEquals(fib,null);
+        Assert.assertEquals(x,1);
+    }
+
+    @Test
+    public void test_fib_2_equals_1(){
+        Fib fib = new Fib();
+
+        int x = fib.DoFib(2);
+
+
+        //Assert.assertEquals(fib,null);
+        Assert.assertEquals(x,1);
+    }
+
+    @Test
+    public void test_fib_3_equals_2(){
+        Fib fib = new Fib();
+
+        int x = fib.DoFib(3);
+
+
+        //Assert.assertEquals(fib,null);
+        Assert.assertEquals(x,2);
+    }
+
+    @Test
+    public void test_fib_4_equals_3(){
+        Fib fib = new Fib();
+
+        int x = fib.DoFib(4);
+
+
+        //Assert.assertEquals(fib,null);
+        Assert.assertEquals(x,3);
+    }
+
+    @Test
+    public void test_fib_5_equals_5(){
+        Fib fib = new Fib();
+
+        int x = fib.DoFib(5);
+
+
+        //Assert.assertEquals(fib,null);
+        Assert.assertEquals(x,5);
+    }
+
+    @Test
+    public void test_fib_6_equals_8(){
+        Fib fib = new Fib();
+
+        int x = fib.DoFib(6);
+
+
+        //Assert.assertEquals(fib,null);
+        Assert.assertEquals(x,8);
+    }
+
 }
 
